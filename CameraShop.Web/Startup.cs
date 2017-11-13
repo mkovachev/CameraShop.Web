cc1d85bed304f52545c8c78d2b7e26a50315ff7d
mkovachev <martin.kovachev@outlook.com>
2017-11-13 21:06:53 +0200

    connection to db refac

--- CameraShop.Web/Startup.cs
+++ CameraShop.Web/Startup.cs
@@ -19,7 +19,6 @@ namespace CameraShop.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CameraShopDbContext>(options =>
@@ -29,13 +28,12 @@ namespace CameraShop.Web
                 .AddEntityFrameworkStores<CameraShopDbContext>()
                 .AddDefaultTokenProviders();
 
-            // Add application services.
+            // Add application services
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
