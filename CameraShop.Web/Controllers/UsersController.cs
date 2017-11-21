0c01c6b8ad72d65e1bc52b23fccace4e93243c75
mkovachev <martin.kovachev@outlook.com>
2017-11-21 22:39:14 +0200

    .

--- CameraShop.Web/Controllers/UsersController.cs
+++ CameraShop.Web/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
 ﻿using CameraShop.Services.Contracts;
+using CameraShop.Services.ServiceModels;
+using CameraShop.Web.Infrastructure;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CameraShop.Web.Controllers
 {
@@ -11,6 +14,33 @@ namespace CameraShop.Web.Controllers
         public UsersController(IUserService users) => this.users = users;
 
         [Authorize]
-        public IActionResult Profile(string id) => View(this.users.GetUserProfile(id));
+        public IActionResult Profile(string id)
+        {
+            var user = this.users.GetUserProfile(id);
+
+            return View(new UserServiceModel
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Cameras = user.Cameras.Select(c => new CameraServiceModel
+                {
+                    Id = c.Id,
+                    Make = c.Make,
+                    Model = c.Model,
+                    Price = c.Price,
+                    Quantity = c.Quantity,
+                    ImageURL = c.ImageURL
+                })
+
+            });
+        }
+
+        [Authorize(Roles = GlobalConstants.AdministratorRole)]
+        public IActionResult All()
+        {
+            return View(this.users.GetUserSWithRoles());
+        }
     }
 }
